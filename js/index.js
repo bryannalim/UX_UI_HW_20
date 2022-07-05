dbdf60494658d3638d05a7fde0a40244ca014ce1
bryannalim <bryannalim@gmail.com>
2022-07-05 01:00:25 -0700

    built out body
    - is there a way to fix the navbar blocking links to same page
    - is there a way to add an interactive carousel to the sections
    - how to fix the dimension ratios of the pictures as the window changes size

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
-function fade() {
-    $('.preloader').fadeOut('slow');
-}
-setTimeout(fade, 3000)
+// function fade() {
+//     $('.preloader').fade('slow');
+// }
+// setTimeout(fade, 3000)
+
+
+// $('#carouselExampleFade').on('slide.bs.carousel', function () {
+//     console.log('carousel next triggered');
+//   })
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+$('.ppvr').popover();
